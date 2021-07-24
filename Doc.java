20fd4e6125b180a18ce4760e555a96aac6f9dc9a
mdyerf <mdyerf1380@gmail.com>
2021-07-24 19:15:57 +0430

    consider a document as object

---
+++ Doc.java
@@ -0,0 +1,22 @@
+import java.util.UUID;
+
+public class Doc {
+
+    private String docID;
+    private String docName;
+    private StringBuilder docText;
+
+    public Doc(String docName, StringBuilder docText) {
+        this.docID = UUID.randomUUID().toString();
+        this.docName = docName;
+        this.docText = docText;
+    }
+
+    public String[] getWords() {
+        return docText.toString().split(" ");
+    }
+    @Override
+    public String toString() {
+        return String.format("{ID: %s, Name: %s}", this.docID.substring(0,5), this.docName);
+    }
+}
