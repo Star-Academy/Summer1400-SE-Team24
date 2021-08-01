87d681c9d37addf6f51c4545345756d0e7c47173
Adib Rezaei <adibrezaeish@gmail.com>
2021-08-01 14:36:08 +0430

    add keyword tests

--- Inverted-Index/src/tests/KeywordsTest.java
+++ Inverted-Index/src/tests/KeywordsTest.java
@@ -1,27 +1,58 @@
 package tests;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
+
 import keywords.Ordinary;
+import keywords.Union;
+import keywords.Exclude;
+import keywords.Keyword;
 import file_handler.Doc;
 
 public class KeywordsTest {
+    private Keyword keyword;
+    private Set<Doc> docs, newDocs, expected_result;
+
+    @Before
+    public void BeforeTest(){
+        docs = new HashSet<Doc>();
+        expected_result = new HashSet<Doc>();
+        docs.add(new Doc("1", Arrays.asList("some", "words", "are", "here")));
+        docs.add(new Doc("2", Arrays.asList("some", "another", "words")));
+        newDocs = new HashSet<Doc>();
+        newDocs.add(new Doc("1", Arrays.asList("some", "words", "are", "here")));
+        newDocs.add(new Doc("4", Arrays.asList("here")));
+    }
     @Test
     public void ordinaryTest(){
-        var ordinary = new Ordinary("word");
-        var docs = new HashSet<Doc>();
-        docs.add(new Doc("1", Arrays.asList("some", "words", "are", "here")));
-        var newDocs = new HashSet<Doc>();
-        newDocs.add(new Doc("1", Arrays.asList("some", "words", "in", "here")));
-        var expected_result = new HashSet<String>();
-        expected_result.add("some");
-        expected_result.add("words");
-        expected_result.add("here");
+        keyword = new Ordinary("word");
+        expected_result.add(new Doc("1", Arrays.asList("some", "words", "are", "here")));
+    }
 
-        var result = ordinary.operate(docs, newDocs);
-        // System.out.println(result.toArray());
-        // Assert.assertEquals(expected_result, result);
+    @Test
+    public void excludeTest(){
+        keyword = new Exclude("word");
+        expected_result.add(new Doc("2", Arrays.asList("some", "another", "words")));  
+    }
+
+    @Test
+    public void includeTest(){
+        keyword = new Union("word");
+        expected_result.add(new Doc("1", Arrays.asList("some", "words", "are", "here")));
+        expected_result.add(new Doc("2", Arrays.asList("some", "another", "words")));
+        expected_result.add(new Doc("4", Arrays.asList("here")));
+    }
+
+    @After
+    public void afterTest(){
+        var result = keyword.operate(docs, newDocs);
+        Assert.assertEquals(expected_result, result);
     }
 }
