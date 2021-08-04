d1969a21c84696e792bbb5d03fd525c412cdf640
mdyerf <mdyerf1380@gmail.com>
2021-08-04 15:26:36 +0430

    mocking mapper in invertedIndex

--- Inverted-Index/src/tests/InvertedIndexTest.java
+++ Inverted-Index/src/tests/InvertedIndexTest.java
@@ -1,20 +1,58 @@
 package tests;
-import java.util.List;
-import org.junit.Test;
-import org.junit.Assert;
+import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.internal.matchers.Any;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.junit.Assert;
+import org.junit.Before;
 
 import file_handler.Doc;
 import search.InvertedIndex;
 import search.Mapper;
 
+@RunWith(MockitoJUnitRunner.class)
 public class InvertedIndexTest {
-    private List<Doc> testDocs;
+
+    private final String KEY = "key";
+    private InvertedIndex index;
+    private Map<String, Set<Doc>> fakeMap;
+    private Set<Doc> docSet;
+
+    @Mock
+    Mapper mapper;
+
+    @Before
+    public void init() {
+
+        index = new InvertedIndex(new ArrayList<>() {
+            {add(new Doc("1", new ArrayList<>()));}
+        }, mapper);
+        docSet = new HashSet<>();
+
+        docSet.add(new Doc("1", new ArrayList<>()));
+        docSet.add(new Doc("2", new ArrayList<>()));
+        docSet.add(new Doc("3", new ArrayList<>()));
+
+        fakeMap = new HashMap<>();
+        fakeMap.put(KEY, docSet);
+    }
 
     @Test
     public void invertedIndexGetTest() {
-        var index = new InvertedIndex(testDocs, new Mapper());
-        var result = index.get("have");
-        Assert.assertEquals(result.size(), 5);
+
+        when(mapper.mergeMaps(any(), any())).thenReturn(fakeMap);
+
+        var result = index.get(KEY);
+        Assert.assertEquals(docSet, result);
     }
 }
