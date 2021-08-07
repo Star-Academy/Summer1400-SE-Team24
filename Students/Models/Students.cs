f4cf7f275c5c238090da438e81ac2a8c62178f01
mdyerf <mdyerf1380@gmail.com>
2021-08-07 07:58:11 +0430

    adding average property to student class

--- Students/Models/Students.cs
+++ Students/Models/Students.cs
@@ -5,5 +5,10 @@ namespace Students.Models
         public int StudentNumber { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public decimal Average { get; set; }
+        public override string ToString()
+        {
+            return $"{StudentNumber}, {FirstName} {LastName}, Average: {Average}";
+        }
     }
 }
--- Students/Services/Students.cs
+++ Students/Services/Students.cs
@@ -6,19 +6,17 @@ namespace Students.Sevices
 {
     public class Students
     {
-        private IList<StudentWithGrade> _students;
+        private IList<Student> _students;
 
         public Students(IList<Student> students, IList<Grade> grades)
         {
-            _students = students.Select(s => new StudentWithGrade() 
+            _students = students.Select(s => 
             {
-                StudentNumber = s.StudentNumber,
-                FirstName = s.FirstName,
-                LastName = s.LastName,
-                Average = grades.Where(g => g.StudentNumber == s.StudentNumber).Average(g => g.Score)
+                s.Average = grades.Where(g => g.StudentNumber == s.StudentNumber).Average(g => g.Score);
+                return s;
             }).ToList();
         }
-        public IList<StudentWithGrade> GetBestAverages(int count)
+        public IList<Student> GetBestAverages(int count)
         {
             return _students.OrderByDescending(s => s.Average).Take(count).ToList();
         }
