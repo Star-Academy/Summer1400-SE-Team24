c626a4e591e820d7b1aa91ebcac25583e8758498
mdyerf <mdyerf1380@gmail.com>
2021-08-07 15:25:41 +0430

    refactoring keyword tests

--- Inverted-Index/src/tests/KeywordsTest.java
+++ Inverted-Index/src/tests/KeywordsTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import keywords.Ordinary;
@@ -17,17 +18,17 @@ import file_handler.Doc;
 public class KeywordsTest {
 
     private Keyword keyword;
-    private Set<Doc> docs, newDocs, expected_result;
+    private Set<Doc> docs, newDocs, expectedResult;
 
-    private final Doc DOC_TEST1 = new Doc("1", Arrays.asList("some", "words", "are", "here"));
-    private final Doc DOC_TEST2 = new Doc("2", Arrays.asList("some", "another", "words"));
-    private final Doc DOC_TEST3 = new Doc("1", Arrays.asList("some", "words", "are", "here"));
-    private final Doc DOC_TEST4 = new Doc("4", Arrays.asList("here"));
+    private final List<String> SAMPLE_WORDS = Arrays.asList("some", "words");
+    private final Doc DOC_TEST1 = new Doc("1", SAMPLE_WORDS);
+    private final Doc DOC_TEST2 = new Doc("2", SAMPLE_WORDS);
+    private final Doc DOC_TEST3 = new Doc("3", SAMPLE_WORDS);
     private final String DUMMY_WORD = "dummyword";
 
     @Before
     public void beforeTest(){
-        expected_result = new HashSet<>();
+        expectedResult = new HashSet<>();
 
         docs = new HashSet<>();
         docs.add(DOC_TEST1);
@@ -35,31 +36,40 @@ public class KeywordsTest {
 
         newDocs = new HashSet<>();
         newDocs.add(DOC_TEST3);
-        newDocs.add(DOC_TEST4);
+        newDocs.add(DOC_TEST1);
     }
+
     @Test
     public void ordinaryTest(){
         keyword = new Ordinary(DUMMY_WORD);
-        expected_result.add(DOC_TEST1);
+        expectedResult.add(DOC_TEST1);
+        var result = keyword.operate(docs, newDocs);
+        Assert.assertEquals(expectedResult, result);
     }
 
     @Test
     public void excludeTest(){
         keyword = new Exclude(DUMMY_WORD);
-        expected_result.add(DOC_TEST2);
+        expectedResult.add(DOC_TEST2);
+        var result = keyword.operate(docs, newDocs);
+        Assert.assertEquals(expectedResult, result);
     }
 
     @Test
     public void includeTest(){
         keyword = new Union(DUMMY_WORD);
-        expected_result.add(DOC_TEST1);
-        expected_result.add(DOC_TEST2);
-        expected_result.add(DOC_TEST4);
+        expectedResult.add(DOC_TEST1);
+        expectedResult.add(DOC_TEST2);
+        expectedResult.add(DOC_TEST3);
+        var result = keyword.operate(docs, newDocs);
+        Assert.assertEquals(expectedResult, result);
     }
 
     @After
     public void afterTest(){
-        var result = keyword.operate(docs, newDocs);
-        Assert.assertEquals(expected_result, result);
+        expectedResult = null;
+        keyword = null;
+        newDocs = null;
+        docs = null;
     }
 }
