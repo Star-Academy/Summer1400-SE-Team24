99cf617c20bb6a76849eb24729f80e5c1c8d164a
mdyErfani <mdyerfani@ut.ac.ir>
2021-08-15 11:57:07 +0430

    removing useless usings

--- Phase11-Backend/Controllers/SearchController.cs
+++ Phase11-Backend/Controllers/SearchController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using InvertedIndex.Data.Entities;
 using InvertedIndex.Data.Services;
 using InvertedIndex.Logic;
 
